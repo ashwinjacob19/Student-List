42dd6005f7965e303cad065f33dbf57e8d019240
ashwinjacob19 <114237046+ashwinjacob19@users.noreply.github.com>
2022-10-09 23:10:26 +0530

    removed a package

--- lib/widgets/add_student_widget.dart
+++ lib/widgets/add_student_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
+// import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_2/db/functions/db_functions.dart';
 import 'package:flutter_application_2/db/model/data_model.dart';
